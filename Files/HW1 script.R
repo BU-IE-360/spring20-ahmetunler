0a1215b1b5bcf160b2643aca6463929569d9e819
ahmetunler <ahmet.unler@boun.edu.tr>
2020-04-22 18:35:27 +0300

    HW 1 Final Updates

--- Files/HW1 script.R
+++ Files/HW1 script.R
@@ -1,8 +1,8 @@
-install.packages("readxl")
-install.packages("xts")
+#install.packages("readxl")
+#install.packages("xts")
 library("readxl")
 library("xts")
-the_data <- read_excel(file.choose())
+the_data <- read_excel("EVDS.xlsx")
 Sys.setlocale(locale= "en_US.UTF-8")
 tarihler <- the_data[,1]
 tarih_mat <- as.matrix.data.frame(tarihler)
@@ -15,3 +15,15 @@ veriler <- the_data[,2:8]
 col_names_vector <- c("bulgar_ziyaretci", "paki_ziyaretci", "kuveyt_ziyaretci", "yab_sey_gelir", "b_leva_oran", "p_rupi_oran", "k_dinar_oran")
 colnames(veriler) <- col_names_vector
 sonveri <- xts(veriler, order.by = date_ver)
+bulg_ziy<- sonveri[, c("bulgar_ziyaretci")]
+bulg_leva <- sonveri[, c("b_leva_oran")]
+plot(bulg_leva)
+plot(bulg_ziy)
+kuw_ziy<- sonveri[, c("kuveyt_ziyaretci")]
+kuw_dinar <- sonveri[, c("k_dinar_oran")]
+plot(kuw_ziy)
+plot(kuw_dinar)
+paki_ziy<- sonveri[, c("paki_ziyaretci")]
+paki_rupi <- sonveri[, c("p_rupi_oran")]
+plot(paki_ziy)
+plot(paki_rupi)
